5e030b49e472964b96978013b47cf22d11ab70ee
Konsta Janhunen <kvjanhun@gmail.com>
2022-03-21 19:54:23 +0200

    Viikko 2: Tehtävä 7

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -3,31 +3,33 @@ from maksukortti import Maksukortti
 
 class TestMaksukortti(unittest.TestCase):
     def setUp(self):
-        self.maksukortti = Maksukortti(10)
+        self.maksukortti = Maksukortti(1000)
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
 
     def test_kortin_saldo_alussa_oikein(self):
-        self.assertEqual(self.maksukortti.saldo, 10)
+        self.assertEqual(self.maksukortti.saldo, 1000)
 
     def test_lataa_rahaa_kasvattaa_saldoa(self):
-        self.maksukortti.lataa_rahaa(12.50)
-        self.assertEqual(self.maksukortti.saldo, 22.50)
+        self.maksukortti.lataa_rahaa(1250)
+        self.assertEqual(self.maksukortti.saldo, 2250)
 
     def test_saldo_vahenee_oikein_kun_rahaa_tarpeeksi(self):
-        self.maksukortti.ota_rahaa(3.50)
-        self.maksukortti.ota_rahaa(2)
-        self.assertEqual(self.maksukortti.saldo, 4.50)
+        self.maksukortti.ota_rahaa(350)
+        self.maksukortti.ota_rahaa(200)
+        self.assertEqual(self.maksukortti.saldo, 450)
 
     def test_saldo_ei_vahene_kun_rahaa_liian_vahan(self):
-        self.maksukortti.ota_rahaa(25)
-        self.assertEqual(self.maksukortti.saldo, 10)
+        self.maksukortti.ota_rahaa(2500)
+        self.assertEqual(self.maksukortti.saldo, 1000)
 
     def test_ota_rahaa_palauttaa_true_kun_rahaa_tarpeeksi(self):
-        self.assertTrue(self.maksukortti.ota_rahaa(2))
+        self.assertTrue(self.maksukortti.ota_rahaa(200))
 
     def test_ota_rahaa_palauttaa_false_kun_rahaa_liian_vahan(self):
-        self.assertFalse(self.maksukortti.ota_rahaa(12))
+        self.assertFalse(self.maksukortti.ota_rahaa(1200))
 
-    
+    def test_saldo_tulostuu_merkkijonona_oikein(self):
+        self.assertEqual(str(self.maksukortti), f"saldo: 10.0")
+        
